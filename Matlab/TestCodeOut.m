032ce3813fd83d27e4989c103b865eccd9afb656
salomon73 <salomon.guinchard@epfl.ch>
2022-10-09 14:01:10 +0200

    Depart

--- Matlab/TestCodeOut.m
+++ Matlab/TestCodeOut.m
@@ -3,11 +3,38 @@ cd /Users/salomonguinchard/Documents/GitHub/SPE_Fall2022/Inputs/Test_ions
 addpath /Users/salomonguinchard/Documents/GitHub/SPE_Fall2022/matlab_routines
 Ions = espic2dhdf5('stable_13_fine.h5');
 
+%% 
+
 Vr = Ions.VR;
 Vz = Ions.VZ;
 
+R = Ions.R;
+Z = Ions.Z;
 
+Ra = 0.001;
+Rb = 0.01;
+Za = -0.32;
+Z_b = 0.32;
 
+dt = Ions.dt;
+nt = R.nt;
+t  = 5*dt*linspace(0,200,nt);
+dr = 1e-3;
+
+npart = R.nparts;
+mat = zeros(npart,nt);
+
+for jj = 1:npart
+    for ii = 1:nt
+        if R(jj,ii) > Rb-dr
+            disp 'touch';
+            mat(jj,ii) = ii;
+        else 
+            mat(jj,ii) = NaN;
+            
+        end
+    end
+end
 
 %% Display particles data
 dispespicParts(Ions);
