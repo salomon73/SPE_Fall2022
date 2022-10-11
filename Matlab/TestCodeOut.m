0d3a053512a5eb831ef619e5c5554b2cfc464a88
salomon73 <salomon.guinchard@epfl.ch>
2022-10-11 22:38:24 +0200

    update energy incident ions

--- Matlab/TestCodeOut.m
+++ Matlab/TestCodeOut.m
@@ -2,8 +2,13 @@
 cd /Users/salomonguinchard/Documents/GitHub/SPE_Fall2022/Inputs/Test_ions
 addpath /Users/salomonguinchard/Documents/GitHub/SPE_Fall2022/matlab_routines
 Ions = espic2dhdf5('stable_13_fine.h5');
+%% Display particles data
+dispespicParts(Ions);
 
-%% 
+%% Display fields data
+dispespicFields(Ions);
+
+%% Collection at electrode using dr interval to model contact %%
 Ions_mass = 3.347e-27;
 Vr = Ions.VR;
 Vz = Ions.VZ;
@@ -31,8 +36,7 @@ R = R(:,:);
 
 for jj = 1:npart
     for ii = 1:nt
-        if R(2,ii) < Ra+partnd
-            disp 'touch';
+        if R(2,ii) < Ra+dr
             mat(jj,ii) = ii; % Trouve l'indice des particles et du temps auquel elles se trouvent pres de/sur l'électrode 
         else 
             mat(jj,ii) = 0;
@@ -49,6 +53,17 @@ for jj =1:npart
         indices(jj,2) = t_ind(1);
     end
 end
+
+%%
+k=150; % Index of particle of interest
+EnergyIon_n = (1/1.602e-19)*0.5*Ions_mass*(VR(k,indices(k,2)).^2+VZ(k,indices(k,2)).^2+VT(k,indices(k,2)).^2);
+
+for n=1:npart
+    E(n) = (1/1.602e-19)*0.5*Ions_mass*(VR(n,indices(n,2)).^2+VZ(n,indices(n,2)).^2+VT(n,indices(n,2)).^2);
+end
+
+figure
+plot(linspace(1,1000,1000),E, 'k+')
 
 %% Follow the particles
 
@@ -80,34 +95,29 @@ R = R(:,:);
 [R0 I] = sort(R(Ions.partindex(:,1),1), 'descend');
 
 POS0 = [I, R0];
- 
+
+%%
 for ii = 1:length(R0)                               % go along all particles array
     for jj = 1:nt                                   % go along all time steps
         if Ions.nbparts(jj) ~= 0                    % check if there are still particles in the cell
             if ismember(ii,Ions.partindex(:,jj))    % check if particle ii still in cell
-
+                continue
+            else 
+                index(ii)  = jj;                 
+                Energy(ii) = (1/1.602e-19)*0.5*Ions_mass*(VR(ii,jj)^2+VZ(ii,jj)^2+VT(ii,jj)^2);
+                EThet(ii)  = (1/1.602e-19)*0.5*Ions_mass*VT(ii,jj)^2;
+                ER(ii)  = (1/1.602e-19)*0.5*Ions_mass*VR(ii,jj)^2;
+                break 
             end
         end
     end
 end
-
-for ii = 1:npart
-    for jj = 1:nt
-        for kk = 1:Ions.nbparts(nt)
-
-        end
-    end
-end
-
 %%
-k=150; % Index of particle of interest
-EnergyIon_n = (1/1.602e-19)*0.5*Ions_mass*(VR(k,indices(k,2)).^2+VZ(k,indices(k,2)).^2+VT(k,indices(k,2)).^2);
-
-for n=1:npart
-    E(n) = (1/1.602e-19)*0.5*Ions_mass*(VR(n,indices(n,2)).^2+VZ(n,indices(n,2)).^2+VT(n,indices(n,2)).^2);
-end
+figure
+plot(R0, Energy, 'ko')
 
 figure
-plot(linspace(1,1000,1000),E, 'k+')
-%% Display particles data
-dispespicParts(Ions);
+plot(R0, EThet./ER, 'ro')
+hold on 
+plot(R0, Energy./ER, 'bo')
+
