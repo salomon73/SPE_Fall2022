363c32dd61397eb1299172361c8e77008a0d9f43
salomon73 <salomon.guinchard@epfl.ch>
2022-10-27 12:40:45 +0200

    pause dejeuner

--- Matlab/Electronic_trajectories.m
+++ Matlab/Electronic_trajectories.m
@@ -3,8 +3,9 @@
 path = '/scratch/sguincha';
 directory = '/SPE_HRES_RUN/ResultsScan3/'; % Change result folder name accordingly
 directory2 = 'SPE_Fall2022/RunsElectronsSlices/Results/';
+directory3 = '/scratch/sguincha/SPE_Fall2022/Results/';
 %-----------------------------------------
-addpath(genpath(strcat(path,directory)));
+addpath(genpath(strcat(path,directory3)));
 addpath(genpath('/home/sguincha/espic2d/matlab/'))
 format long 
 
@@ -16,14 +17,23 @@ nbSpecies  = species(2);
 Rel        = electrons.species(end).R;
 R          = Rel(:,:);
 nparts     = length(R(:,1));
-npartalloc = 1;
+npartalloc = 10;
 tpart      = electrons.species(end).tpart;
+ngyrations = 5;
 %% RUN test - isparticle leaving electrode ? %%
 for i = 1:npartalloc
-    LocalMinInd(i,:) = islocalmin(1e3*R(i,:));  % test if each time step has local min of elect traject
-    index(i,:)       = find(LocalMinInd(i,:));  % find all local min indices
-    diff(i)          = R(i,index(i,1))-R(i,1);
-    isfarenough(i)   = (diff(i)>1e-8);
+    
+    LocalMinInd(i,:) = islocalmin(1e3*R(i,:)); % test if each time step has local min of elect traject
+    
+    if isempty(find(LocalMinInd(i,:), 1,'first'))
+        isfarenough(i) = 1;
+        index(i)       = NaN;
+        break
+    else 
+        index(i)         = find(LocalMinInd(i,:), 1,'first');  % find first local min index
+        diff(i)          = R(i,index(i))-R(i,1);
+        isfarenough(i)   = (diff(i)>1e-8);
+    end
 end
 
 %% PLOT R trajectory if farenough ==1 %%
@@ -34,7 +44,7 @@ for ii = 1:npartalloc
             figure
                 plot(1e6*tpart,1e3*R(ii,:), 'b-', 'linewidth', 2);
                 hold on
-                plot(1e6*tpart(index(ii,1:3)), 1e3*R(ii,index(ii,1:3)), 'r*' )
+                plot(1e6*tpart(index(ii)), 1e3*R(ii,index(ii)), 'r*' )
                 ylabel('$r_e$ [mm]', 'interpreter', 'latex','Fontsize', 22)
                 xlabel('$t$ [$\mu$s]', 'interpreter', 'latex', 'Fontsize', 22)
                 set (gca, 'fontsize', 22)
@@ -53,6 +63,7 @@ for j = 1:npartalloc
     for k= 2:length(tpart)
         if R(j,k) == R(j,k-1)
             timeStep(j) = k-1;
+        break 
         else 
             timeStep(j)=k;
         end
