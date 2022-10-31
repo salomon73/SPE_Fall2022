1b99c44fee6b46424df79ecab3c7e2b3fb36e441
salomon73 <salomon.guinchard@epfl.ch>
2022-10-31 18:31:24 +0100

    initial radial and axial velocities file

--- Matlab/initialiseMaxwellVzVr.m
+++ Matlab/initialiseMaxwellVzVr.m
@@ -9,14 +9,13 @@
 
     lowerBound = 0.1;
     upperBound = 20;
-    nPoints = 20;
+    nPoints = 10;
     cathode = geom.spl_bound.boundary(1);
-    LowPts = 15;
-    UpPts  = 65;
+    LowPts  = 15;
+    UpPts   = 65;
     
     f   = cathode.fun;
     E   = linspace(lowerBound, upperBound, nPoints);
-    vR  = sqrt((2*e/m)*E);
     Points = zeros(UpPts-LowPts,2);
     Tang   = zeros(UpPts-LowPts,2);
     Norm   = zeros(UpPts-LowPts,2);
@@ -37,7 +36,7 @@
 
     LowInd  = LowPts-1;
     HighInd = UpPts-1;
-        figure
+    figure
         hold on 
         plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
         ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
@@ -47,6 +46,114 @@
         plot(Points(1:3:end,1),Points(1:3:end,2),'r*')
         hold on 
         quiver(Points(1:3:end,1),Points(1:3:end,2), Norm(1:3:end,1), Norm(1:3:end,2), 'b')
+        axis equal
         
         
+    fileId = fopen(strcat(textFile,'.txt'),'w');
+    fprintf(fileId,'//parts\n');
+    for ii =1:length(E)
+        for jj =1:UpPts-LowPts
+            fprintf(fileId,'%.8f %.1f %.8f %.8f %.1f %.8f \n', Points(jj,1), 0.0, Points(jj,2), Norm(jj,1), 0.0 ,Norm(jj,2));
+        end
+    end
+    fclose(fileId);
+    
+    
+    %%
+
+format long
+nppts = UpPts-LowPts;
+vR    = zeros(length(E),nppts); 
+vZ    = zeros(length(E),nppts); 
+
+for kk=1:length(E)    
+    vRmax = sqrt(2*E(kk)/m*e);
+    vR(kk,:)    = linspace(0,vRmax, nppts);
+    
+    for j = 1 :length(vR(kk,:))
+        if 2*E(kk)/m*e - vR(kk,j)^2 < 1e-3
+            vZ(kk,j) = 0.0;
+        else 
+            vZ(kk,j) = sqrt(2*E(kk)/m*e - vR(kk,j)^2);
+        end
+    end
+end    
+
+V0R = zeros(length(E),nppts);
+V0Z = zeros(length(E),nppts);
+
+for ii = 1: length(E)
+    
+    V0R(ii,:) = Norm(:,2)'.*vR(ii,:);
+    V0Z(ii,:) = Norm(:,1)'.*vZ(ii,:);
+    
+end
+
+    figure
+        hold on 
+        plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
+        ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
+        xlabel('$Z$ [m]', 'interpreter', 'latex', 'Fontsize', 22)
+        set (gca, 'fontsize', 20)
+        hold on 
+        plot(Points(1:3:end,1),Points(1:3:end,2),'r*')
+        hold on 
+        quiver(Points(1:3:end,1),Points(1:3:end,2), V0Z(1,1:3:end)', V0R(1,1:3:end)', 'b')
+        axis equal
+
+    figure
+        plot(1:nppts,1/(e*2)*m*(vR(1,:).^2+vZ(1,:).^2),'linewidth', 2)
+        hold on 
+        plot(1:nppts,1e-5*vR(1,:), 'linewidth', 2)
+        hold on 
+        plot(1:nppts,1e-5*vZ(1,:), 'linewidth', 2)
+        legend('E', 'vR','vZ', 'Location','northwest','Interpreter','latex');
+        set(legend,'FontSize',18);
+        set (gca, 'fontsize', 22)
+
+    figure
+        subplot(2,2,1)
+            hold on 
+            plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
+            ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
+            xlabel('$Z$ [m]', 'interpreter', 'latex', 'Fontsize', 22)
+            set (gca, 'fontsize', 20)
+            hold on 
+            plot(Points(1:3:end,1),Points(1:3:end,2),'r.', 'markersize',12)
+            hold on 
+            quiver(Points(1:3:end,1),Points(1:3:end,2), V0Z(1,1:3:end)', V0R(1,1:3:end)', 'b')
+            axis equal
+        subplot(2,2,2)
+            hold on 
+            plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
+            ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
+            xlabel('$Z$ [m]', 'interpreter', 'latex', 'Fontsize', 22)
+            set (gca, 'fontsize', 20)
+            hold on 
+            plot(Points(1:3:end,1),Points(1:3:end,2),'r.', 'markersize',12)
+            hold on 
+            quiver(Points(1:3:end,1),Points(1:3:end,2), V0Z(5,1:3:end)', V0R(5,1:3:end)', 'b')
+            axis equal
+        subplot(2,2,3)
+            hold on 
+            plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
+            ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
+            xlabel('$Z$ [m]', 'interpreter', 'latex', 'Fontsize', 22)
+            set (gca, 'fontsize', 20)
+            hold on 
+            plot(Points(1:3:end,1),Points(1:3:end,2),'r.', 'markersize',12)
+            hold on 
+            quiver(Points(1:3:end,1),Points(1:3:end,2), V0Z(10,1:3:end)', V0R(10,1:3:end)', 'b')
+            axis equal
+        subplot(2,2,4)
+            hold on 
+            plot(cathode.coefs(:,1),cathode.coefs(:,2), 'k-', 'linewidth',2) % plot cathode geometry
+            ylabel('$R$ [m]', 'interpreter', 'latex','Fontsize', 22)
+            xlabel('$Z$ [m]', 'interpreter', 'latex', 'Fontsize', 22)
+            set (gca, 'fontsize', 20)
+            hold on 
+            plot(Points(1:3:end,1),Points(1:3:end,2),'r.', 'markersize',12)
+            quiver(Points(1:3:end,1),Points(1:3:end,2), V0Z(end,1:3:end)', V0R(end,1:3:end)', 'b')
+            axis equal
+            
         
