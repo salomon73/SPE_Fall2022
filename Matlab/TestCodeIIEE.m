ebbbac012b4c866ccf0defdf825e00e5c495aba4
salomon73 <salomon.guinchard@epfl.ch>
2022-11-29 16:52:15 +0100

    Test normal velocity passed

--- Matlab/TestCodeIIEE.m
+++ Matlab/TestCodeIIEE.m
@@ -1,3 +1,4 @@
+%% generates paths and output structure %% 
 addpath( '/home/sguincha/espic2d/matlab/')
 addpath(genpath('/Users/salomonguinchard/Documents/GitHub/SPE_Fall2022/Matlab/Data/'))
 
@@ -5,7 +6,8 @@ filename = 'Test_V0_H_SS.h5';
 ions = espic2dhdf5(filename);
 dt = ions.dt;
 time = dt*linspace(0,double(ions.nrun),ions.nrun+1);
-%% 
+
+%% plot number of particles over time %% 
 figure
     plot(time, ions.nbparts, 'linewidth', 2)
     hold on 
@@ -25,3 +27,21 @@ figure
     legend('$n_i$', '$n_e$' ,'Location','best','Interpreter','latex');
     set(legend,'FontSize',18);
     set (gca, 'fontsize', 22)
+
+%% Collect information about generated electrons %%
+R = ions.species(1).R;
+R = R(:,:);
+R = R(1:25,:);
+Z = ions.species(1).Z;
+Z = Z(:,:);
+Z = Z(1:25,:);
+
+%% Checks that initial electron velocity is normal %% 
+figure
+    plot(Z(2,76:end), R(2,76:end), 'k-', 'linewidth', 2)
+    xlabel('Z', 'Interpreter', 'Latex') 
+    ylabel('R', 'Interpreter', 'Latex')
+    set (gca, 'fontsize', 22)
+   
+figure
+    plot(Z(2,76:end), 'k-')
