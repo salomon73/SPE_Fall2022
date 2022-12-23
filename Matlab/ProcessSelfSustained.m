bc888dd1f85714285d2c4f99c940162ba406aa44
salomon73 <salomon.guinchard@epfl.ch>
2022-12-23 16:18:55 +0100

    ions current density processing

--- Matlab/ProcessSelfSustained.m
+++ Matlab/ProcessSelfSustained.m
@@ -11,10 +11,11 @@
 %% process surf flux for electronic current
 
     subdiv=1;
+    species_id=1; %electrons
     timestep = length(ions.t2d)-1000;
     mflux= ions.Metallicflux(timestep,subdiv);
-    lflux= -squeeze(ions.Axialflux(timestep,1))';
-    rflux= squeeze(ions.Axialflux(timestep,length(ions.zgrid)))';
+    lflux= -squeeze(ions.Axialflux(timestep,1,species_id))';
+    rflux= squeeze(ions.Axialflux(timestep,length(ions.zgrid),species_id))';
 
     time=ions.t2d(timestep);
 
@@ -102,12 +103,14 @@
     plot(peak2, 'r-')
    
  %% several points - all %%
+ % good for H2Slanted TREX geometry %
     compteur = 0;
+    species_id = 1; % electrons
     for ii=2500:50:length(ions.t2d)
         compteur = compteur +1;
         mflux= ions.Metallicflux(ii,subdiv);
-        lflux= -squeeze(ions.Axialflux(ii,1))';
-        rflux= squeeze(ions.Axialflux(ii,length(ions.zgrid)))';
+        lflux= -squeeze(ions.Axialflux(ii,1,species_id))';
+        rflux= squeeze(ions.Axialflux(ii,length(ions.zgrid), species_id))';
 
         time=ions.t2d(ii);
 
@@ -120,7 +123,8 @@
         temps(compteur) = time;
 %         peak1(ii) = flux(Ind(end));
 %         peak2(ii) = flux(Ind(end-1));
-    end            time=obj.t2d(timestep);
+    end
+
 %%
     avg1 = smoothdata(peak1);
     avg2 = smoothdata(peak2);
@@ -144,11 +148,53 @@
     
 %% process the surf flux for ionic current
 
-    mflux= obj.MetallicFlux_species(timestep,subdiv);
-    lflux= -squeeze(obj.Axialflux(timestep,1,species_id))';
-    rflux= squeeze(obj.Axialflux(timestep,length(obj.zgrid),species_id))';
-    time=obj.t2d(timestep);
+    species_id = 2;
+    mflux= ions.MetallicFlux_species(timestep,subdiv);
+    lflux= -squeeze(ions.Axialflux(timestep,1,species_id))';
+    rflux= squeeze(ions.Axialflux(timestep,length(ions.zgrid),species_id))';
+    time=ions.t2d(timestep);
+    qe = ions.species(species_id-1).q;
+    ids=1:length(mflux);
     
-    
-    
-    
+            P=ions.neutcol.neutdens*ions.kb*300/100;% pressure at room temperature in mbar
+            f=figure('name','fluxevol');
+            linew=5;
+            %ions.displaysplbound(gca,1e3);
+            contour(ions.zgrid*1e3,ions.rgrid*1e3,ions.geomweight(:,:,1),[0 0],'b-','linewidth',1.5);
+            hold on
+            for i=1:length(mflux.p)
+                x=mflux.p{i}(1,:)*1000;
+                y=mflux.p{i}(2,:)*1000;
+                y(end)=NaN;
+                %c=mflux.gamma{i}'*ions.qe/(100^2)/P;
+                c=mflux.gamma{i}'*qe/(100^2)/P;
+                c(c<=0)=NaN;
+                patch(x,y,c,'EdgeColor','interp','LineWidth',linew);
+                hold on
+            end
+            
+            x=ions.zgrid(1)*ones(size(ions.rgrid))*1000;
+            y=ions.rgrid*1000;
+            y(end)=NaN;
+            %c=lflux*ions.qe/(100^2)/P;
+            c=lflux*qe/(100^2)/P;
+            c(c<=0)=NaN;
+            patch(x,y,c,'EdgeColor','interp','LineWidth',linew);
+            
+            
+            x=ions.zgrid(end)*ones(size(ions.rgrid))*1e3;
+            y=ions.rgrid*1000;
+            y(end)=NaN;
+            %c=rflux*ions.qe/(100^2)/P;
+            c=rflux*qe/(100^2)/P;
+            c(c<=0)=NaN;
+            patch(x,y,c,'EdgeColor','interp','LineWidth',linew);
+            
+            title(sprintf('t=%4.2f [ns]',time*1e9))
+            
+            c=colorbar;
+            c.Label.String= 'j\cdotn [A/(cm^2 mbar)]';
+            xlabel('z [mm]')
+            ylabel('r [mm]')
+            colormap(jet)
+            set(gca,'colorscale','log')
