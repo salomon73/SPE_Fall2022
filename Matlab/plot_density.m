f5c548131c1c11fee7b2a71fe13ed6ec27b05d68
salomon73 <salomon.guinchard@epfl.ch>
2023-01-08 17:21:58 +0100

    last gt170 plots

--- Matlab/plot_density.m
+++ Matlab/plot_density.m
@@ -87,20 +87,20 @@
                 contour(ions_less.zgrid,ions_less.rgrid,Blines,real(levels),'m-.','linewidth',1.5);
                 set (gca, 'fontsize', 20)
         subplot(1,2,2)
-                fieldstep = length(ions_2.t2d);
+                fieldstep = length(ions.t2d);
                 fixed = false;
                 logdensity=false;
                 showgrid=false;
-                dens=ions_2.N(:,:,fieldstep);
-                dens(ions_2.geomweight(:,:,1)<0)=NaN;
+                dens=ions.N(:,:,fieldstep);
+                dens(ions.geomweight(:,:,1)<0)=NaN;
                 %[~,sf]=contourf(ax1,ions_less.zgrid,ions_less.rgrid,dens,40,'edgecolor','none');
                 if(showgrid)
-                [sf]=surface(ions_2.zgrid,ions_2.rgrid,dens);
+                [sf]=surface(ions.zgrid,ions.rgrid,dens);
                 else
-                [~,sf]=contourf(ions_2.zgrid,ions_2.rgrid,dens,40,'edgecolor','none');
+                [~,sf]=contourf(ions.zgrid,ions.rgrid,dens,40,'edgecolor','none');
                 end
-                xlim([ions_2.zgrid(1) ions_2.zgrid(end)])
-                ylim([ions_2.rgrid(1) ions_2.rgrid(end)])
+                xlim([ions.zgrid(1) ions.zgrid(end)])
+                ylim([ions.rgrid(1) ions.rgrid(end)])
                 xlabel('z [m]')
                 ylabel('r [m]')
                 title('Density')
@@ -120,10 +120,10 @@
                 colormap(hotmap);
                 hold on
 
-                contour(ions_2.zgrid,ions_2.rgrid,ions_2.geomweight(:,:,1),[0 0],'r-','linewidth',1.5);
+                contour(ions.zgrid,ions.rgrid,ions.geomweight(:,:,1),[0 0],'r-','linewidth',1.5);
 
                 Blines=ions_less.rAthet;
-                levels=linspace(min(Blines(ions_2.geomweight(:,:,1)>0)),max(Blines(ions_2.geomweight(:,:,1)>0)),20);
-                Blines(ions_2.geomweight(:,:,1)<0)=NaN;
-                contour(ions_2.zgrid,ions_2.rgrid,Blines,real(levels),'m-.','linewidth',1.5);
+                levels=linspace(min(Blines(ions.geomweight(:,:,1)>0)),max(Blines(ions.geomweight(:,:,1)>0)),20);
+                Blines(ions.geomweight(:,:,1)<0)=NaN;
+                contour(ions.zgrid,ions.rgrid,Blines,real(levels),'m-.','linewidth',1.5);
                 set (gca, 'fontsize', 20)
